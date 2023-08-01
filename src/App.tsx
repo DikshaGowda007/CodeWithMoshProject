f8b11e019286c303001cdec35eb3872b054d6058
Diksha Gowda <diksha.gowda@webenetic.com>
2023-08-01 13:03:53 +0530

    Adding a Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const App = () => {
   return (
     <>
+    <div className="container-fluid mx-5 my-5">
+      <Button colorScheme='blue'>Button</Button>
+    </div>
     </>
   )
 }
