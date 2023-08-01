806daf2ec867d9def75088d9d0b354c797715a84
Diksha Gowda <diksha.gowda@webenetic.com>
2023-08-01 13:15:25 +0530

    Creating a Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import 'bootstrap/dist/css/bootstrap.css'
-
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 const App = () => {
   return (
     <>
-    <div className="container-fluid mx-5 my-5">
-      <Button colorScheme='blue'>Button</Button>
-    </div>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`     //1024px
+    }}>
+    <GridItem area='nav' bg='coral'>Nav</GridItem>
+    <Show above='lg'>
+      <GridItem area='aside' bg='gold'>Aside</GridItem>
+    </Show>
+    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
     </>
   )
 }
