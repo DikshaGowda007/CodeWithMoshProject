bc6bd7121f3589df761c19c005623078d3ce359d
Diksha Gowda <diksha.gowda@webenetic.com>
2023-08-03 16:32:13 +0530

    Building Search Input

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,15 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { BsSearch } from "react-icons/bs"
+
+const SearchInput = () => {
+  return (
+    <>
+        <InputGroup>
+          <InputRightElement children={<BsSearch />} />
+            <Input borderRadius={20} placeholder='Search games...' variant='filled' />
+        </InputGroup>
+    </>
+  )
+}
+
+export default SearchInput
