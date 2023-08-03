846e224c73be80ae4f1f2096c615dd17da63e321
Diksha Gowda <diksha.gowda@webenetic.com>
2023-08-03 14:53:21 +0530

    Refactoring: Extracting a Query Object

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,13 @@ import { useState } from 'react'
 import { Genre } from './hooks/useGenres'
 import PlatformSelector from './components/PlatformSelector'
 import { Platform } from './hooks/useGames'
+
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
 const App = () => {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
   return (
     <>
     <Grid templateAreas={{
@@ -25,13 +29,13 @@ const App = () => {
     <Show above='lg'>
       <GridItem area='aside' paddingX={5}>
         <GenreList onSelectGenre={(genre)=>{
-          setSelectedGenre(genre);
-        }} selectedGenre={selectedGenre} />
+          setGameQuery({...gameQuery, genre});
+        }} selectedGenre={gameQuery.genre} />
       </GridItem>
     </Show>
     <GridItem area='main'>
-      <PlatformSelector selectedPlatform={selectedPlatform} onSelectPlatform={(platform)=>{ setSelectedPlatform(platform)}} />
-      <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
+      <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>{ setGameQuery({...gameQuery ,platform })}} />
+      <GameGrid gameQuery={gameQuery} />
     </GridItem>
     </Grid>
     </>
--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -4,7 +4,8 @@ interface Props {
     score: number,
 }
 const CriticScore = ({ score }: Props) => {
-  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : 'red';
+  // let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
   return (
     <>
     <Badge fontSize="14px" padding={2} borderRadius="4px" colorScheme={color}>{score}</Badge>
